97e9d8643714617c901d0f6bcfeb66ef21c66e21
Guilherme Domingues <guilhermegdmd@gmail.com>
2025-05-23 15:45:34 -0300

    Removendo arquivos corrompidos

--- backend/Controle/ControleEstac.py
+++ backend/Controle/ControleEstac.py
@@ -2,10 +2,10 @@ from Entidades.VagasComum import VagasComum
 from Entidades.Veiculo import Veiculo
 
 
-class ControleEst:
+class ControleEstac:
      def __init__(self):
           pass
      
      def addVeiculo(self, Veiculo: Veiculo, VagasComum: VagasComum):
-          print(f"Veículo adicionado: {Veiculo.modelo} \nVaga: VagasComum.localizacao")
+          print(f"Veículo adicionado: {Veiculo.modelo} \nVaga: {VagasComum.localizacao}")
           pass
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from Controle import ControleEst
+from Controle.ControleEstac import ControleEstac
 from Entidades.Veiculo import Veiculo
 from Entidades.VagasComum import VagasComum
 
-controle = ControleEst()
+controle = ControleEstac()
 veiculo = Veiculo(1, "placa", "modelo", "cor", "dataEntrada", "horaEntrada")
 vaga = VagasComum(2, "localizacao", "categoria", "valor")
 controle.addVeiculo(veiculo, vaga)
